8749b051590339c89799d0d5d7a06bf92a04a553
chuga23101996 <cuong.le@digimed.vn>
2019-08-27 18:34:50 +0700

    remove fallback reducer

--- src/reducers/fallbackReducer.js
+++ src/reducers/fallbackReducer.js
@@ -1,15 +1,15 @@
-import { FallbackTypes } from '@types';
+// import { FallbackTypes } from '@types';
 
 
-const fallbackReducer = (state = null, response) => {
-  switch (response.type) {
-    case FallbackTypes.FALLBACK_ERROR:
-      return response.action
-    case FallbackTypes.FALLBACK_RETRY:
-      return null
-    default:
-      return state
-  }
-}
+// const fallbackReducer = (state = null, response) => {
+//   switch (response.type) {
+//     case FallbackTypes.FALLBACK_ERROR:
+//       return response.action
+//     case FallbackTypes.FALLBACK_RETRY:
+//       return null
+//     default:
+//       return state
+//   }
+// }
 
-export default fallbackReducer
+// export default fallbackReducer
